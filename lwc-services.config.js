579df23fdeb1648fa91f436664f44dba5ac33320
mshanemc <shane.mclaughlin@salesforce.com>
2020-10-22 14:30:13 -0500

    no lwc

--- lwc-services.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    localModulesDirs: ['./node_modules/@mshanemc/lwc-oss-base/src/modules'],
-    sourceDir: './src/client',
-    resources: [{ from: 'src/client/resources', to: 'dist/resources' }],
-    devServer: {
-        proxy: { '/': 'http://localhost:8443' }
-    }
-};
