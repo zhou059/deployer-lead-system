0e240ecc1291ecd1dab300c09c1f0cc493c6efb8
mshanemc <shane.mclaughlin@salesforce.com>
2020-12-08 13:45:04 -0600

    new env vars for oauth username/password flow

--- src/server/lib/processWrapper.ts
+++ src/server/lib/processWrapper.ts
@@ -1,9 +1,16 @@
 const processWrapper = {
     PORT: process.env.PORT ?? 8443,
     sfdcLeadCaptureServlet: process.env.sfdcLeadCaptureServlet,
+    sfdcLeadCaptureUsername: process.env.sfdcLeadCaptureUsername,
+    sfdcLeadCapturePassword: process.env.sfdcLeadCapturePassword,
+    sfdcLeadCaptureServletAuth: process.env.sfdcLeadCaptureServletAuth,
+    sfdcLeadCaptureClientSecret: process.env.sfdcLeadCaptureClientSecret,
+    sfdcLeadCaptureClientId: process.env.sfdcLeadCaptureClientId,
+
     LEAD_QUEUE: process.env.LEAD_QUEUE ?? 'leads',
     FAILED_QUEUE: process.env.FAILED_QUEUE ?? 'failedLeads',
-    REDIS_URL: process.env.REDIS_URL
+    REDIS_URL: process.env.REDIS_URL,
+    FIXIE_URL: process.env.FIXIE_URL
 };
 
 export { processWrapper };
